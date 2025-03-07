62d07b3115283ea8b18e1b0175f7e38727c23795
sajakshrestha8 <shresthasajak123@gmail.com>
2025-03-07 22:23:04 +0545

    optimized the code

--- task-14/index.js
+++ task-14/index.js
@@ -1,12 +1,19 @@
+// output -> 0, 1, 1, 2, 3, 5, 8, 13, 21
+//formula -> n3 = n1 + n2
+
 let n1 = 0;
 let n2 = 1;
 
-console.log(n1);
+console.log(n1, "\n", "s");
 console.log(n2);
 
-for (let i = 0; i <= 10; i++) {
-  let temp = n1 + n2;
+function fibonnaci() {
+  let n3 = n1 + n2;
   n1 = n2;
-  n2 = temp;
-  console.log(temp);
+  n2 = n3;
+  return n3;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(fibonnaci());
 }
