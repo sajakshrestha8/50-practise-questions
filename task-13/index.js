4e63aba9a82388fd48f21769acc918eb0895b10c
sajakshrestha8 <shresthasajak123@gmail.com>
2025-03-07 22:43:39 +0545

    optmized task-13

--- task-13/index.js
+++ task-13/index.js
@@ -1,18 +1,10 @@
+//given array =  [ 1, 4, 21, 25, 26, 28, 34, 45, 98, 100, 56, 57, 32, 33, 62, 66, 70, 78, 79, 83];
+//output => maximum Number => 100
+
 const arr = [
   1, 4, 21, 25, 26, 28, 34, 45, 98, 100, 56, 57, 32, 33, 62, 66, 70, 78, 79, 83,
 ];
 
-function maximum() {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] < arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-    }
-  }
-  console.log(arr[0]);
-}
+let descendingOrder = arr.sort((a, b) => b - a);
 
-maximum();
+console.log(descendingOrder[0]);
