67aebf69a5298536b4a889f02a561cf2bc39f648
sajakshrestha8 <shresthasajak123@gmail.com>
2025-03-07 22:01:55 +0545

    task-15

--- task-15/index.js
+++ task-15/index.js
@@ -1,11 +1,14 @@
-let n1 = 0;
-let n2 = 1;
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+//using recurssion
 
-function fibonacci(n) {
-  if (n < 2) {
-    return n;
-  }
-  return fibonacci(n - 1) + fibonacci(n - 2);
+// formula for fibonnaci series
+// f(n) = f(n-1) + f(n-2)
+
+function fibonnaci(n) {
+  if (n <= 1) return n;
+  return fibonnaci(n - 1) + fibonnaci(n - 2);
 }
 
-console.log(fibonacci(10));
+for (let i = 0; i < 10; i++) {
+  console.log(fibonnaci(i));
+}
